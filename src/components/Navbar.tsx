0cacf161c13ad59eaa0466c8e4f03bc3bfa52e1a
Dandymay <dandyainul19@gmail.com>
2024-02-09 00:32:36 +0700

    navbar test

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <>
-      <h1 className="underline text-3-xl bg-slate-500">halo</h1>
+      <nav className="flex p-4 bg-sky-800">
+        <h1 className="w-1/5 text-3xl font-bold text-white ">Movie App</h1>
+        <div className="flex items-center justify-center w-3/5 gap-10">
+          <NavLink className="inline-block" to="/">
+            Dashboard
+          </NavLink>
+          <NavLink className="inline-block" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="inline-block" to="/about">
+            About
+          </NavLink>
+        </div>
+        <div className="flex justify-end w-1/5 ">
+          <button className="px-3 py-2 bg-white rounded-xl">Login</button>
+        </div>
+      </nav>
     </>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Movies from "./pages/Movies.tsx";
+import About from "./pages/About.tsx";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/movies",
+    element: <Movies />,
+  },
+  {
+    path: "/about",
+    element: <About />,
   },
 ]);
 
